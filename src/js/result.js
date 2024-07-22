b39f65c4f0938c93b44babed6b73f933a821b9e5
Saltyy <clement.bouret@epitech.eu>
2024-07-23 01:45:40 +0200

    feat: using a string to store names and use switch case for github links

--- src/js/result.js
+++ src/js/result.js
@@ -3,24 +3,40 @@ function getQueryParam(param) {
     return urlParams.get(param);
 }
 
+const validNames = ['alexandre', 'yanis', 'nicolas', 'tom', 'léo', 'julien'];
 const name = getQueryParam('name');
-if (name) {
+
+if (name && validNames.includes(name.toLowerCase())) {
     const greeting = document.getElementById('greeting');
     greeting.textContent = `Joyeux anniversaire ${name.charAt(0).toUpperCase() + name.slice(1)} !`;
 
     const personImage = document.getElementById('personImage');
-    personImage.src = `assets/${name}.jpg`;
+    personImage.src = `assets/${name.toLowerCase()}.jpg`;
 
     const githubLink = document.getElementById('githubLink');
-    if (name === 'alexandre') {
-        githubLink.href = 'https://github.com/ludecieldev';
-    } else if (name === 'nicolas') {
-        githubLink.href = 'https://github.com/NicoDumetz';
-    } else if (name === 'yanis') {
-        githubLink.href = 'https://github.com/Yasl290';
-    } else if (name === 'tom') {
-        githubLink.href = 'https://github.com/FukuInTheCode';
-    } else {
-        githubLink.style.display = 'none';
+    switch (name.toLowerCase()) {
+        case 'alexandre':
+            githubLink.href = 'https://github.com/ludecieldev';
+            break;
+        case 'nicolas':
+            githubLink.href = 'https://github.com/NicoDumetz';
+            break;
+        case 'yanis':
+            githubLink.href = 'https://github.com/Yasl290';
+            break;
+        case 'tom':
+            githubLink.href = 'https://github.com/FukuInTheCode';
+            break;
+        case 'léo':
+            githubLink.href = 'https://github.com/leodumont444';
+            break;
+        case 'julien':
+            githubLink.href = 'https://github.com/Julienmarss';
+            break;
+        default:
+            githubLink.style.display = 'none';
+            break;
     }
+} else {
+    window.location.href = 'invalid.html';
 }
